033058ce95ff6dc9719763a60b9ba8eb07f04229
Esthuardo Quispe <quispefesthuardo@gmail.com>
2023-07-04 20:27:15 -0500

    Forma de escritorio completa, forma movil al 80%

---
+++ semana05/dia02-JavaScript/app.js
@@ -0,0 +1,20 @@
+console.log("Hola chicos soy el Java script")
+console.log('Hola mundo')
+console.log(5*2)
+console.log(10/3)
+//módulo =>residuo (se usa el %)//
+console.log(10%3)
+console.log(typeof(3==5));
+//Conversion automatica
+let age=28; //Para variables locales
+let nombre
+nombre='Gianmarco'
+var apellido //para variables globales
+
+apellido='Flores'
+console.log(nombre + ' ' + apellido)
+//Sugerencias camelCase PascalCase snake_case
+let first_name='Mariana'
+//usar el ingles ;_;
+let age2=12
+//usar nombres descriptivos
