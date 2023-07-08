3b52c3a87338837dd1c070fc8581ff39c0ac3695
Esthuardo Quispe <quispefesthuardo@gmail.com>
2023-07-07 19:55:46 -0500

    ajustes con :hover

---
+++ semana06/dia02-javascript basico/index.js
@@ -0,0 +1,18 @@
+console.log('Hola js');
+let descuento=false;
+console.log(descuento);
+console.log('go');
+// EJERCICIOS
+// 1. Retorna True si  dos strings tienen la misma longitud sino devolver False
+let nombre1='Javier'
+let nombre2='Andres'
+let longitud= nombre1.length == nombre2.length
+console.log(longitud)
+nombre2='Ana'
+longitud= nombre1.length == nombre2.length
+console.log(longitud)
+// 2. Retornar True si un numero es menor que 40 sino devolver False
+
+// 3. Retornar True si un numero es menor que 60 sino devolver False
+// 4. Retornar True si un numero es par sino devolver False
+// 5. Retornar True si un numero es impar sino devolver False
